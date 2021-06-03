8c5b373d5326237645078de46ebd212e2c7fd751
shubhangthaker <77440319+shubhangthaker@users.noreply.github.com>
2021-06-03 15:50:23 +0530

    Add files via upload

---
+++ iron.js
@@ -0,0 +1,26 @@
+class Iron{
+	constructor(x,y,)
+	{
+	// assign options to the rubber ball
+		this.x=x;
+		this.y=y;
+		this.body=Bodies.rectMode(this.x, this.y, )
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var options = {
+                'restitution':0.8,
+                'friction':3,
+                'density':30
+                
+            }
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			
+			//draw the ellipse here to display the rubber ball
+
+Pop()
